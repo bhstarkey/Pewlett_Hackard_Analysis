df9724ba74fb306b6d68f9fdc869883a511ee2e3
bhstarkey <116474586+bhstarkey@users.noreply.github.com>
2022-12-19 12:18:52 -0700

    Updated queries throufh 7.3.5

--- queries.sql
+++ queries.sql
@@ -95,3 +95,57 @@ GROUP BY de.dept_no
 ORDER BY de.dept_no
 ;
 
+-- 1) emp_info
+SELECT * 
+FROM dept_emp
+;
+
+SELECT
+	e.emp_no,
+	e.last_name,
+	e.first_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees AS e
+INNER JOIN salaries AS s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no
+;
+
+-- 2) manager_info
+SELECT
+	d.dept_name,
+	dm.dept_no,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date,
+	ce.first_name,
+	ce.last_name
+INTO manager_info
+FROM departments AS d
+	INNER JOIN dept_manager AS dm
+	  ON (d.dept_no = dm.dept_no)
+	INNER JOIN current_emp as ce
+	  ON (ce.emp_no = dm.emp_no)
+;
+
+-- 3) dept_retirees
+SELECT
+	ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp AS ce
+INNER JOIN dept_emp AS de
+ON ce.emp_no = de.emp_no
+INNER JOIN departments AS d
+ON de.dept_no = d.dept_no
+;
