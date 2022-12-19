de200c2a3a0759f19201f44c88cd3742d889db9e
bhstarkey <116474586+bhstarkey@users.noreply.github.com>
2022-12-19 14:21:35 -0700

    Added missing query to challenge file for Deliverable 1

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,4 +1,19 @@
 -- Deliverable 1
+SELECT
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees AS e
+	INNER JOIN titles AS t
+	  ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no
+;
+
 SELECT DISTINCT ON (emp_no) emp_no,
 	first_name,
 	last_name,
